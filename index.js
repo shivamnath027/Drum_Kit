70f5557b9d21a420bd430b83019071b77cfc6bed
Shivam Nath <shivamnath027@gmail.com>
2023-08-25 11:07:55 +0530

    DRUM

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// document.querySelector("button").addEventListener("click",handleClick);
+//if handleclick()-->  calling function when eventListener is added.
+//if handleclick --> calling function when click happens
+
+//write everything at the call position "ANONYMOUS FUNCTION" ok bye
+
+
+
+var numberofdrumbuttons= document.querySelectorAll(".drum").length;
+for(var i=0;i<numberofdrumbuttons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+}
+
+
+function handleClick(){
+    // alert("I got clicked");
+    // console.log(this);
+    //this.style.color="white";
+    var buttonInner= this.innerHTML;
+    switch(buttonInner){
+        case "w":   var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+        case "a":   var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+        case "s":   var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+        case "d":   var tom4 = new Audio("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+        case "j":   var audio = new Audio("sounds/crash.mp3");
+                    audio.play();
+                    break;
+        case "k":   var audio = new Audio("sounds/kick-bass.mp3");
+                    audio.play();
+                    break;
+        case "l":   var audio = new Audio("sounds/snare.mp3");
+                    audio.play();
+                    break;
+        default:    console.log(buttonInner);
+    }
+}
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//     audio.play();
+
+//Higher ORder Functions
+//That can take other functions as inputs
+
+
+// CONSTRUCTOR FUNCTION-->
+
+// function BellBoy(name,age){
+//     this.name=name;
+//     this.age=age;
+// }
+
+// var bellboy = new BellBoy("Raj",22);
